c82bd5fd00021f4dd3b154f8d2f70b7d57095a5c
Yi <iammini5@gmail.com>
2017-06-06 20:52:25 +1200

    adjust item space

--- app/src/main/java/com/yzeng/hackernews/util/AppConstants.java
+++ app/src/main/java/com/yzeng/hackernews/util/AppConstants.java
@@ -3,5 +3,5 @@ package com.yzeng.hackernews.util;
 
 public class AppConstants extends Constants {
 
-    public static final int RECYCLER_VIEW_ITEM_SPACE = 48;
+    public static final int RECYCLER_VIEW_ITEM_SPACE = 20;
 }
